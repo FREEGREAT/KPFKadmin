bd78e7ad3f4e21278cb16f712ae070e9bddd7a07
FREEGREAT <vladdmitrenko3009@gmail.com>
2023-06-10 22:57:43 +0300

    commit

---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>
+        Zalupka
+      </h1>
+      <div>
+            <div>
+                <h2>Car1</h2>
+                <p>100 000$</p>
+                <button>Read more</button>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
